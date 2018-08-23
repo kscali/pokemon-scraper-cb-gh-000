4ceaa8ec1e1cb08331630fb14f6404f48768e7d0
Koy Saeteurn <kosatun@aol.com>
2018-08-23 20:33:47 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -14,8 +14,8 @@ class Pokemon
     @@all 
   end 
   
-  def self.save(id, name, type, db) 
-    db.execute("INSERT INTO pokemon(id, name, type, db) VALUES(?,?,?,?)", id, name, type, db)
+  def self.save(name, type, db) 
+    db.execute("INSERT INTO pokemon(name, type, db) VALUES(?,?,?,?)", id, name, type, db)
     
   end   
   
