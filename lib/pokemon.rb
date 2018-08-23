2d1982a76ac4d1daecbc899b03b717daeb36b061
Koy Saeteurn <kosatun@aol.com>
2018-08-23 20:38:30 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,6 +19,8 @@ class Pokemon
   end   
   
   def self.find 
+    pokemon_info = db.execute("SELECT * FROM pokemon WHERE id=?", id_num).flatten
+    Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
     
   end   
   
